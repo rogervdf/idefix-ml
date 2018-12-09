cf60b13b87fce6f1bcba7ea36c03b19c1e9a4f71
rogervdf <43315295+rogervdf@users.noreply.github.com>
2018-12-09 13:53:32 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name = 'idefixx',
+    #packages = ['idefix-ml'], #dependencies
+    version = '0.11',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Identical image crosscheck',
+    author = 'Roger van Daalen',
+    author_email = 'roger@vandaalen.eu',
+    url = 'https://github.com/rogervdf/idefix-ml/',
+    #download_url = 'https://github.com/rogervdf/idefix-ml.git',
+    classifiers = ["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",],
+    )
